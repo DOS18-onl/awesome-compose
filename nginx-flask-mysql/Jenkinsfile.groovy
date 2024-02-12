d856bb7bb576541aaf7ab480edab3d59e9b0b6a1
Max Razumov <maxmax.razumov@gmail.com>
2024-02-12 19:01:43 +0100

    Update Jenkinsfile.groovy
    Signed-off-by: Max Razumov <maxmax.razumov@gmail.com>

--- nginx-flask-mysql/Jenkinsfile.groovy
+++ nginx-flask-mysql/Jenkinsfile.groovy
@@ -1,30 +1,40 @@
-pipeline {
-    agent {
-      label "docker"
-    }
+// pipeline {
+//     agent {
+//       label "docker"
+//     }
 
-    stages {
-        stage('Build backend image') {
-            steps {
-                echo 'Building backend image'
-                docker.build("flaskbackend", "-f ./backend/Dockerfile")
-            }
-        }
-        stage('Build nginx image') {
-            steps {
-                echo 'Building nginx image'
-                def nginxImage = docker.build("nginx", "-f ./proxy/Dockerfile")
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
+//     stages {
+//         stage('Build backend image') {
+//             steps {
+//                 echo 'Building backend image'
+//                 docker.build("flaskbackend", "-f ./backend/Dockerfile")
+//             }
+//         }
+//         stage('Build nginx image') {
+//             steps {
+//                 echo 'Building nginx image'
+//                 def nginxImage = docker.build("nginx", "-f ./proxy/Dockerfile")
+//             }
+//         }
+//         stage('Test') {
+//             steps {
+//                 echo 'Testing..'
+//             }
+//         }
+//         stage('Deploy') {
+//             steps {
+//                 echo 'Deploying....'
+//             }
+//         }
+//     }
+// }
+
+node (label: "build") {
+    stage("build nginx image") {
+        def customImage = docker.build("nginx-image:${env.BUILD_ID}", "./proxy/Dockerfile")
+
+        customImage.inside {
+            sh 'echo yoyo'
         }
     }
 }
