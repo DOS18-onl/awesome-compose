d9f27fdd62d61ce0ca4072ff0a6bc66067d7bc51
Max Razumov <maxmax.razumov@gmail.com>
2024-02-12 18:47:19 +0100

    Update Jenkinsfile.groovy
    Signed-off-by: Max Razumov <maxmax.razumov@gmail.com>

--- nginx-flask-mysql/Jenkinsfile.groovy
+++ nginx-flask-mysql/Jenkinsfile.groovy
@@ -7,13 +7,13 @@ pipeline {
         stage('Build backend image') {
             steps {
                 echo 'Building backend image'
-                backendImage = docker.build("flaskbackend", "./backend/Dockerfile")
+                backendImage = docker.build("flaskbackend", "-f ./backend/Dockerfile")
             }
         }
         stage('Build nginx image') {
             steps {
                 echo 'Building nginx image'
-                def nginxImage = docker.build("nginx", "./proxy/Dockerfile")
+                def nginxImage = docker.build("nginx", "-f ./proxy/Dockerfile")
             }
         }
         stage('Test') {
