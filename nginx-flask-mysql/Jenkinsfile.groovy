e0ef4bc081dd29d4e797d69fe6729ef0910d4886
Max Razumov <maxmax.razumov@gmail.com>
2024-02-17 02:57:23 +0100

    Update Jenkinsfile.groovy
    Signed-off-by: Max Razumov <maxmax.razumov@gmail.com>

--- nginx-flask-mysql/Jenkinsfile.groovy
+++ nginx-flask-mysql/Jenkinsfile.groovy
@@ -36,7 +36,7 @@ node (label: "docker") {
          abc = "sdf"
          checkout scm
         sh "ls"
-         def dockerImage = docker.build("nginx-image:${env.BUILD_ID}", "nginx-flask-mysql/backend/Dockerfile")
+         def dockerImage = docker.build("nginx-image:${env.BUILD_ID}", "nginx-flask-mysql/backend/")
 
     //     customImage.inside {
     //         sh 'echo yoyo'
