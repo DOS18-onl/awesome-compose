4691e0cadc08fa2d7971dbc127bcf1d9d8612bb6
koshcheii <151556716+koshcheii@users.noreply.github.com>
2024-02-17 14:34:38 +0300

    Fix Jenkinsfile.groovy
    Signed-off-by: koshcheii <151556716+koshcheii@users.noreply.github.com>

--- flask/Jenkinsfile.groovy
+++ flask/Jenkinsfile.groovy
@@ -1,6 +1,6 @@
 node(label: "docker") {
   stage ("build") {
     checkout scm // склонировать репозиторий 
-    docker.build("flask-app:123123123", "flask/app") --искать докер файл в flask/app
+    docker.build("flask-app:123123123", "flask/app") // искать докер файл в flask/app
   }
 }
