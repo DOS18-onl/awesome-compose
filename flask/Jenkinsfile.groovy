a3527f3205c3207ffa6293ba4ae9e4dd0cb6ca6b
Evgene <95587607+hitmouz@users.noreply.github.com>
2024-02-19 19:35:09 +0300

    Update Jenkinsfile.groovy
    Signed-off-by: Evgene <95587607+hitmouz@users.noreply.github.com>

--- flask/Jenkinsfile.groovy
+++ flask/Jenkinsfile.groovy
@@ -2,6 +2,6 @@ node(label: "docker") {
   stage ("build") {
     checkout scm                  //копирование репозитория к себе
 
-    docker.build("flask-app:400401", "flask/app")
+    docker.build("flask-app1:400402", "flask/app")
   }
 }  
